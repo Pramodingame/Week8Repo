6c5df241d1a34d5f3d357d6bb45ec09781f94ac9
Milton-Pulley <milton.pulley@students.mq.edu.au>
2024-09-10 16:09:32 +1000

    Imported new input system. Added player movement.

--- Week8prac/Assets/Scripts/Player.cs
+++ Week8prac/Assets/Scripts/Player.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int moveSpeed;
+    
+    private Vector3 movementVector;
+
+    private PlayerActions playerActions;
+    private InputAction moveAction;
+
+    private void OnEnable()
     {
-        
+        moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+    }
+
+    // Awake is called once the instance is created
+    private void Awake()
+    {
+        playerActions = new PlayerActions();
+        moveAction = playerActions.playerControls.move;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Works as the z component will just be 0 (see line 740 of Unity's Vector2.cs).
+        movementVector = moveAction.ReadValue<Vector2>();
+        // Swap y and z components
+        movementVector.z = movementVector.y;
+        movementVector.y = 0.0f;
+
+        transform.Translate(movementVector * moveSpeed * Time.deltaTime);
     }
 }
